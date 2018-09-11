c0c10ed19d0f777d5da6950ccb0b47bca5c364ce
Infinity2010 <arbiter2008@ya.ru>
2018-09-11 15:25:03 +0300

    create new package for Lesson1

--- src/ru/com/GeekBrains/Lesson1/MainClass.java
+++ src/ru/com/GeekBrains/Lesson1/MainClass.java
@@ -1,19 +1,21 @@
+package ru.com.GeekBrains.Lesson1;
+
 public class MainClass {
     public static void main(String[] args) {
 
-    // п.2
-        byte b = 12;                // 1 байт - от -128 до 127
-        short s = 250;              // 2 байта - от -32768 до 32767
-        int i = 45000;              // 4 байта - от -2147483648 до 2147483647
-        long l = 452654l;           // 8 байт - от -(2 в 64 степени) до (2 в 64 степени)-1
+    // Рї.2
+        byte b = 12;                // 1 пїЅпїЅпїЅпїЅ - пїЅпїЅ -128 пїЅпїЅ 127
+        short s = 250;              // 2 пїЅпїЅпїЅпїЅпїЅ - пїЅпїЅ -32768 пїЅпїЅ 32767
+        int i = 45000;              // 4 пїЅпїЅпїЅпїЅпїЅ - пїЅпїЅ -2147483648 пїЅпїЅ 2147483647
+        long l = 452654l;           // 8 пїЅпїЅпїЅпїЅ - пїЅпїЅ -(2 пїЅ 64 пїЅпїЅпїЅпїЅпїЅпїЅпїЅ) пїЅпїЅ (2 пїЅ 64 пїЅпїЅпїЅпїЅпїЅпїЅпїЅ)-1
 
-        float f = 1.0f;             // 4 байта
-        double d = 12.3;            // 8 байт
+        float f = 1.0f;             // 4 пїЅпїЅпїЅпїЅпїЅ
+        double d = 12.3;            // 8 пїЅпїЅпїЅпїЅ
 
-        char c = 'c';               // 2 байта - отдельный символ в UTF-16
+        char c = 'c';               // 2 пїЅпїЅпїЅпїЅпїЅ - пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ UTF-16
         char c2 = 'b';
 
-        boolean boolTrue = true;    // true или false
+        boolean boolTrue = true;    // true пїЅпїЅпїЅ false
         boolean boolFalse = false;
 
 
@@ -41,42 +43,42 @@ public class MainClass {
 
     }
 
-    // п.3
+    // Рї.3
     public static int getSolution(int a, int b, int c, int d) {
         return a * (b + (c / d));
     }
 
-    //п.4
+    //Рї.4
     public static boolean getSolution2Digits(int a, int b) {
         if ((a + b) >= 10 && (a + b) <= 20) {
             return true;
         } else return false;
     }
 
-    //п.5
+    //Рї.5
     public static void isPositiveOrNegative(int a) {
         if (a < 0) {
-            System.out.println("Число отрицательное");
-        } else System.out.println("Число положительное");
+            System.out.println("пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
+        } else System.out.println("пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
     }
 
-    //п.6
+    //Рї.6
     public static boolean isNegative(int a) {
         if (a < 0) {
             return true;
         } else return false;
     }
 
-    //п.7
+    //Рї.7
     public static void getGreetings(String s) {
-        System.out.println("Привет, " + s);
+        System.out.println("РџСЂРёРІРµС‚, " + s);
     }
 
-    //п.8
+    //Рї.8
     public static void isLeapYear(int year) {
         if (year % 100 == 0 && year % 400 != 0 || year % 4 != 0) {
-            System.out.println("Год невисокосный");
-        } else System.out.println("Год високосный");
+            System.out.println("пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
+        } else System.out.println("пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
 
     }
 
