75efe2fdb5eb9ad72e138d86d52864c6dfc7896a
Infinity2010 <arbiter2008@ya.ru>
2018-09-12 11:21:27 +0300

    сделал задачу Угадай слово

--- src/ru/com/GeekBrains/Lesson3/GuessTheWord.java
+++ src/ru/com/GeekBrains/Lesson3/GuessTheWord.java
@@ -22,8 +22,42 @@ package ru.com.GeekBrains.Lesson3;
     Используем только маленькие буквы
 */
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheWord {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
+        String[] words = {
+                "apple", "orange", "lemon", "banana", "apricot", "avocado",
+                "broccoli", "carrot", "cherry", "garlic", "grape", "melon",
+                "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
+                "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        String word = words[new Random().nextInt(words.length)];
+        System.out.println("Я загадал слово. Угадай, какое!");
+        String guess;
+        int temp;                                                                           // при попытке угадать сюда записываем длину более короткого слова
+        char[] tempChar = {'x','x','x','x','x','x','x','x','x','x','x','x','x','x','x'};    // массив символов - маска
+        do {
+            guess = input.nextLine().toLowerCase();
+            if (guess.equals(word)) {
+                break;
+            } else {
+                temp = guess.length() <= word.length() ? guess.length() : word.length();
+                for (int i = 0; i < temp; i++) {
+                    if (guess.charAt(i) == word.charAt(i)) tempChar[i] = guess.charAt(i);
+                }
+            }
+            System.out.println(tempChar);
+
+        } while (!guess.equals(word));
+        System.out.println("Молодец!");
+
+
 
     }
+
+
 }
