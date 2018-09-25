a92a5f41e84012bf62b9c8f15f39e6db3e9495f4
Infinity2010 <arbiter2008@ya.ru>
2018-09-25 22:35:42 +0300

    перенёс инициализацию переменных в конструктор и добавил комментарии к коду

--- src/ru/com/GeekBrains/Lesson7/Cat.java
+++ src/ru/com/GeekBrains/Lesson7/Cat.java
@@ -2,10 +2,18 @@ package ru.com.GeekBrains.Lesson7;
 
 public class Cat {
     private String name;
-    private int appetite;
-    private int saturationFood = 0;
-    private int catFullness;
-    private boolean fullness = false;
+    private int appetite;                   // сколько кот съедает за один подход к тарелке
+    private int saturationFood;             // насыщение кота едой - сколько кот съел единиц еды - увеличивается на appetite за каждый подход к тарелке
+    private int catFullness;                // сколько коту нужно съесть до полного насыщения
+    private boolean fullness;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.saturationFood = 0;                    // насыщение кота едой на нуле
+        this.fullness = false;                      // кот изначально голодный
+        this.catFullness = this.appetite * 4;
+    }
 
     public int getSaturationFood() {
         return saturationFood;
@@ -27,23 +35,15 @@ public class Cat {
         return fullness;
     }
 
-    public Cat(String name, int appetite) {
-        this.name = name;
-        this.appetite = appetite;
-        this.catFullness = this.appetite * 4;
-    }
-
     public void eat(Plate p) {
-        if (!this.fullness) {
-            if (p.decreaseFood(this.appetite)) {
-                System.out.printf("%s съел %d единиц еды\n", this.name, this.appetite);
-                this.saturationFood += this.appetite;
-                if (this.saturationFood >= this.catFullness) {
-                    //System.out.printf("%s наелся\n", this.name);
-                    this.fullness = true;
-                }
-            } else {
-                System.out.printf("%s не смог съесть %d единиц еды\n", this.name, this.appetite);
+        if (this.fullness) {
+            return;
+        }
+        if (p.decreaseFood(this.appetite)) {
+            System.out.printf("%s съел %d единиц еды\n", this.name, this.appetite);
+            this.saturationFood += this.appetite;
+            if (this.saturationFood == this.catFullness) {                              // если кот насытился до состояния насыщения -
+                this.fullness = true;                                                   // устанавливаем насыщение в true
             }
         }
     }
