c9cf62c47f0ca2500001f1acef48ad9c77c85294
Infinity2010 <arbiter2008@ya.ru>
2018-09-27 00:44:42 +0300

    первое оконное приложение

--- src/ru/com/GeekBrains/Lesson7/MyFrame/MyFrame.java
+++ src/ru/com/GeekBrains/Lesson7/MyFrame/MyFrame.java
@@ -1,64 +1,127 @@
-package ru.com.GeekBrains.Lesson7.WinApp;
+package ru.com.GeekBrains.Lesson7.MyFrame;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
-import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
-import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+class SimpleWindow extends JFrame {
+    private JLabel lblSurname;
+    private JLabel lblName;
+    private JLabel lblMidname;
+    private JLabel lblSurnameText;
+    private JLabel lblNameText;
+    private JLabel lblMidnameText;
+    private JButton btnFillName;
+    private JButton btnExit;
 
-public class MyFrame extends JFrame {
-    private JLabel lbl1;
-    private JLabel lbl2;
-    private JLabel lbl3;
-    private JButton btn;
-    private MyHandler hnd;
-
-    public MyFrame(String title) {
+    public SimpleWindow(String title) {
         super(title);
-        setBounds(250, 250, 350, 250);
+        int width = 350, height = 250;
+        setBounds(400, 300, width, height);
         setResizable(false);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
-        lbl1 = new JLabel("Фамилия: ");
-        lbl1.setBounds(10, 10, 230, 50);
-        add(lbl1);
-        lbl2 = new JLabel("Имя: ");
-        lbl2.setBounds(10, 50, 230, 50);
-        add(lbl2);
-        lbl3 = new JLabel("Отчество: ");
-        lbl3.setBounds(10, 90, 230, 50);
-        add(lbl3);
-        btn = new JButton("Закрыть окно");
-        btn.setBounds(50, 170, 120, 30);
-        hnd = new MyHandler();
-        btn.addActionListener(hnd);
-        add(btn);
-        //lbl3.setText("Изменили текст");
+
+        lblSurname = new JLabel();
+        lblSurname.setText("Фамилия: ");
+        lblSurname.setBounds(10, 10, width / 2 - 20, 50);
+        add(lblSurname);
+
+        lblName = new JLabel();
+        lblName.setText("Имя: ");
+        lblName.setBounds(10, 50, width / 2 - 20, 50);
+        add(lblName);
+
+        lblMidname = new JLabel();
+        lblMidname.setText("Отчество: ");
+        lblMidname.setBounds(10, 90, width / 2 - 20, 50);
+        add(lblMidname);
+
+        lblSurnameText = new JLabel();
+        lblSurnameText.setText("... ");
+        lblSurnameText.setBounds(lblSurname.getWidth() + 20, 10, width / 2 - 10, 50);
+        add(lblSurnameText);
+
+        lblNameText = new JLabel();
+        lblNameText.setText("... ");
+        lblNameText.setBounds(lblName.getWidth() + 20   , 50, width / 2 - 10, 50);
+        add(lblNameText);
+
+        lblMidnameText = new JLabel();
+        lblMidnameText.setText("... ");
+        lblMidnameText.setBounds(lblMidname.getWidth() + 20, 90, width / 2 - 10, 50);
+        add(lblMidnameText);
+
+        btnFillName = new JButton();
+        btnFillName.setText("Заполнить ФИО");
+        btnFillName.setBounds(10, height - 80, 150, 30);
+        btnFillName.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int width = 350, height = 250;
+                JDialog innerDialog = new JDialog();
+                innerDialog.setModal(true);
+                innerDialog.setTitle("Модальное окно");
+                innerDialog.setBounds(500, 400, width, height);
+                innerDialog.setResizable(false);
+                innerDialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+                innerDialog.setLayout(null);
+
+                JTextField txtSurname = new JTextField();
+                txtSurname.setBounds(10, 10, width - 35, 25);
+                txtSurname.setText("");
+                innerDialog.add(txtSurname);
+
+                JTextField txtName = new JTextField();
+                txtName.setBounds(10, txtSurname.getHeight() + 25, width - 35, 25);
+                txtName.setText("");
+                innerDialog.add(txtName);
+
+                JTextField txtMidname = new JTextField();
+                txtMidname.setBounds(10, txtSurname.getHeight() + txtName.getHeight() + 40, width - 35, 25);
+                txtMidname.setText("");
+                innerDialog.add(txtMidname);
+
+                JButton btnOk = new JButton();
+                btnOk.setText("Заполнить ФИО");
+                btnOk.setBounds(width / 2 - 85, height - 80, 150, 30);
+                btnOk.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (txtSurname.getText().equalsIgnoreCase("") || txtName.getText().equalsIgnoreCase("") || txtMidname.getText().equalsIgnoreCase("")) {
+                            innerDialog.setIconImage(innerDialog.getToolkit().getImage("E:\\GeekBrains\\src\\ru\\com\\GeekBrains\\Lesson7\\MyFrame\\attention.png"));
+                            innerDialog.setTitle("Заполните все поля!");
+                        } else {
+                            lblSurnameText.setText(txtSurname.getText());
+                            lblNameText.setText(txtName.getText());
+                            lblMidnameText.setText(txtMidname.getText());
+                            innerDialog.setVisible(false);
+                        }
+
+                    }
+                });
+                innerDialog.add(btnOk);
+
+                innerDialog.setVisible(true);
+            }
+        });
+        add(btnFillName);
+
+        btnExit = new JButton("Выход");
+        btnExit.setBounds(btnFillName.getWidth() + 25, height - 80, 150, 30);
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        add(btnExit);
+
         setVisible(true);
     }
 }
 
-class MyHandler implements ActionListener {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        JDialog inFrame = new JDialog();
-        inFrame.setModal(true);
-        inFrame.setTitle("Модальное окно");
-        inFrame.setBounds(300, 300, 350, 250);
-        inFrame.setResizable(false);
-        inFrame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        inFrame.setLayout(null);
-        JTextField txtField = new JTextField();
-        inFrame.add(txtField);
-        inFrame.setVisible(true);
-        System.out.println("Button pressed...");
-    }
-}
-
-class SimpleWindow {
+public class MyFrame {
     public static void main(String[] args) {
-        new MyFrame("Обычное окно");
+        new SimpleWindow("Главное окно");
     }
 }
