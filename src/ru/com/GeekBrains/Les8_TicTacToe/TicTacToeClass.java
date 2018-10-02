2b321d39c0b41e8869e7ad465fa8a80c8040622d
Infinity2010 <arbiter2008@ya.ru>
2018-10-02 22:24:40 +0300

    - добавлена установка native look and feel в главный метод программы - обработчик UI в отдельном потоке (invokeLater)

--- src/ru/com/GeekBrains/Les8_TicTacToe/TicTacToeClass.java
+++ src/ru/com/GeekBrains/Les8_TicTacToe/TicTacToeClass.java
@@ -1,7 +1,20 @@
 package ru.com.GeekBrains.Les8_TicTacToe;
 
+import javax.swing.*;
+
 public class TicTacToeClass {
     public static void main(String[] args) {
-        new GameWindow();
+        // For Native Look and Feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch(Exception ignored){}
+
+        /* Run the Frame */
+        /* Базовый механизм запуска произвольного кода в потоке GUI — invokeLater */
+        SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                new GameWindow();
+            }
+        });
     }
 }
